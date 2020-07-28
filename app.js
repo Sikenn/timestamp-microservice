a2ad7946b5c081c89d94c6d55d7ba60542ce4b5f
Siken <sikenn@pm.me>
2020-07-28 13:55:32 +0200

    Adding process.env.port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use('/api/timestamp', router);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
 	console.log(`app is listening on ${port}`)
